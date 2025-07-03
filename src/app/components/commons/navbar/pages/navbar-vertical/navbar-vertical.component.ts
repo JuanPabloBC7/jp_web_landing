64675ad252c2614560ec9d7a7a5b0cf253f1866f
JuanPabloBC7 <juanpablobc7@gmail.com>
2025-07-02 21:45:20 -0600

    Traduciendo home y menu en navbar top

--- src/app/components/commons/navbar/pages/navbar-vertical/navbar-vertical.component.ts
+++ src/app/components/commons/navbar/pages/navbar-vertical/navbar-vertical.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MenuItemComponent } from './menu-item/menu-item.component';
 
 @Component({
@@ -7,6 +8,7 @@ import { MenuItemComponent } from './menu-item/menu-item.component';
   standalone: true,
   imports: [
     CommonModule,
+    TranslateModule,
     MenuItemComponent
   ],
   templateUrl: './navbar-vertical.component.html',
@@ -26,252 +28,209 @@ export class NavbarVerticalComponent {
    * events: Events of elemnt.                        [object]        optional
    */
 
-  menu: any = [
-    { 
-      "path": "", 
-      "label": "Inicio", 
-      "type": "link", 
-      "icon": "fa-solid fa-house", 
-      "divider": true, 
-      "dividername": "Inicio", 
-    }, 
-    { 
-      "path": "about-us", 
-      "label": "Sobre mi", 
-      "type": "link", 
-      "icon": "fa-solid fa-user", 
-    },
-    { 
-      "path": "skills-&-tools", 
-      "label": "Habilidades y Herramientas", 
-      "type": "link", 
-      "icon": "fa-solid fa-bolt-lightning", 
-    }, 
-    { 
-      "path": "works", 
-      "label": "Trabajos", 
-      "type": "link", 
-      "icon": "fa-solid fa-clock-rotate-left", 
-    },  
-    { 
-      "path": "notes", 
-      "label": "Notas", 
-      "type": "link", 
-      "icon": "fa-solid fa-book", 
-      "newLabel": "me",
-      "newStyle": "text-primary-emphasis bg-primary-subtle border border-primary-subtle",
-    }, 
-    { 
-      "path": "experience", 
-      "label": "Experience", 
-      "type": "link", 
-      "icon": "fa-solid fa-medal", 
-      "divider": true, 
-      "dividername": "Experience", 
-      "newLabel": "important",
-      "newStyle": "text-warning-emphasis bg-warning-subtle border border-warning-subtle",
-    }, 
-    { 
-      "path": "plugins-n-tools", 
-      "label": "Plugis y Herramientas", 
-      "type": "link", 
-      "icon": "fa-solid fa-screwdriver-wrench", 
-      "newLabel": "new",
-      "newStyle": "text-success-emphasis bg-success-subtle border border-success-subtle",
-    }, 
-    // { 
-    //   "path": "", 
-    //   "label": "HTML", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    //   "divider": true, 
-    //   "dividername": "Frontend", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "Angular", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "React", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "ASP.NET", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "JavaScript", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    //   "divider": true, 
-    //   "dividername": "Backend", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "TypeScript", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "Python", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "PHP", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "Java", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "C#", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "Visual Basic", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "SQL Server", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    //   "divider": true, 
-    //   "dividername": "DataBase", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "MySQL", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "Oracle", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "MongoDB", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "MariaDB", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "PostgreSQL", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
-    // { 
-    //   "path": "", 
-    //   "label": "Firebase", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // }, 
+  // menu: any = [
+  //   { 
+  //     "path": "", 
+  //     "label": "HTML", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //     "divider": true, 
+  //     "dividername": "Frontend", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "Angular", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "React", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "ASP.NET", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "JavaScript", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //     "divider": true, 
+  //     "dividername": "Backend", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "TypeScript", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "Python", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "PHP", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "Java", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "C#", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "Visual Basic", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "SQL Server", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //     "divider": true, 
+  //     "dividername": "DataBase", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "MySQL", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "Oracle", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "MongoDB", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "MariaDB", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "PostgreSQL", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
+  //   { 
+  //     "path": "", 
+  //     "label": "Firebase", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   }, 
     
 
 
 
-    // { 
-    //   "path": "", 
-    //   "label": "Simple example", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    // },
-    // { 
-    //   "path": "", 
-    //   "label": "Example with divider", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    //   "divider": true, 
-    //   "dividername": "Divider title", 
-    // },
-    // { 
-    //   "path": "", 
-    //   "label": "Example with new tag", 
-    //   "type": "link", 
-    //   "newLabel": "new",
-    //   "newStyle": "text-bg-success",
-    //   "icon": "fa-solid fa-house" 
-    // },
-    // { 
-    //   "path": "", 
-    //   "label": "Example with multi level", 
-    //   "type": "sub", 
-    //   "icon": "fa-solid fa-house", 
-    //   "expanded": false, 
-    //   "children": [
-    //     { 
-    //       "path": "lvl/lvl1", 
-    //       "label": "Level 1", 
-    //       "type": "link", 
-    //     }, 
-    //     { 
-    //       "path": "lvl/lvl2", 
-    //       "label": "Level 2", 
-    //       "type": "sub", 
-    //       "expanded": false, 
-    //       "children": [
-    //         { 
-    //           "path": "lvl2/lvl21", 
-    //           "label": "Level 3", 
-    //           "type": "link", 
-    //         }, 
-    //         { 
-    //           "path": "lvl2/lvl22", 
-    //           "label": "Level 4", 
-    //           "type": "sub", 
-    //           "expanded": false, 
-    //           "children": [
-    //             { 
-    //               "path": "lvl3/lvl31", 
-    //               "label": "Level 5", 
-    //               "type": "link", 
-    //             }, 
-    //             { 
-    //               "path": "lvl3/lvl32", 
-    //               "label": "Level 6", 
-    //               "type": "link", 
-    //             }
-    //           ] 
-    //         }
-    //       ] 
-    //     }
-    //   ] 
-    // },
-    // { 
-    //   "path": "", 
-    //   "label": "Example with event", 
-    //   "type": "link", 
-    //   "icon": "fa-solid fa-house", 
-    //   "event": 1, 
-    //   "events": { "click": "logout" } // view menu-item.component.ts
-    // },
-  ];
+  //   { 
+  //     "path": "", 
+  //     "label": "Simple example", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //   },
+  //   { 
+  //     "path": "", 
+  //     "label": "Example with divider", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //     "divider": true, 
+  //     "dividername": "Divider title", 
+  //   },
+  //   { 
+  //     "path": "", 
+  //     "label": "Example with new tag", 
+  //     "type": "link", 
+  //     "newLabel": "new",
+  //     "newStyle": "text-bg-success",
+  //     "icon": "fa-solid fa-house" 
+  //   },
+  //   { 
+  //     "path": "", 
+  //     "label": "Example with multi level", 
+  //     "type": "sub", 
+  //     "icon": "fa-solid fa-house", 
+  //     "expanded": false, 
+  //     "children": [
+  //       { 
+  //         "path": "lvl/lvl1", 
+  //         "label": "Level 1", 
+  //         "type": "link", 
+  //       }, 
+  //       { 
+  //         "path": "lvl/lvl2", 
+  //         "label": "Level 2", 
+  //         "type": "sub", 
+  //         "expanded": false, 
+  //         "children": [
+  //           { 
+  //             "path": "lvl2/lvl21", 
+  //             "label": "Level 3", 
+  //             "type": "link", 
+  //           }, 
+  //           { 
+  //             "path": "lvl2/lvl22", 
+  //             "label": "Level 4", 
+  //             "type": "sub", 
+  //             "expanded": false, 
+  //             "children": [
+  //               { 
+  //                 "path": "lvl3/lvl31", 
+  //                 "label": "Level 5", 
+  //                 "type": "link", 
+  //               }, 
+  //               { 
+  //                 "path": "lvl3/lvl32", 
+  //                 "label": "Level 6", 
+  //                 "type": "link", 
+  //               }
+  //             ] 
+  //           }
+  //         ] 
+  //       }
+  //     ] 
+  //   },
+  //   { 
+  //     "path": "", 
+  //     "label": "Example with event", 
+  //     "type": "link", 
+  //     "icon": "fa-solid fa-house", 
+  //     "event": 1, 
+  //     "events": { "click": "logout" } // view menu-item.component.ts
+  //   },
+  // ];
+  menu: any = [];
 
+  constructor(
+    private translateServices: TranslateService,
+  ) { 
+    this.translateServices.stream('menu').subscribe(res => {
+      this.menu = [];
+      this.menu = res;
+    });
+  }
 }
--- src/app/pages/pages/home/home.component.ts
+++ src/app/pages/pages/home/home.component.ts
@@ -1,15 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ContactUsComponent } from '../../landing-page/contact-us/contact-us.component';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    ContactUsComponent
+    CommonModule,
+    TranslateModule,
+    ContactUsComponent,
+    NgbTooltipModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  translateSocial: any = {};
+  socialMedia: any = [];
 
+  constructor(
+    private translateServices: TranslateService,
+  ) { 
+    this.translateServices.stream('configuration.social').subscribe(res => {
+      this.translateSocial = res;
+      this.socialMedia = [];
+
+      for (const key in this.translateSocial.links) {
+        switch (key) {
+          case 'facebook':
+          case 'instagram':
+          case 'whatsapp':
+          case 'telegram':
+          case 'twitter':
+          case 'github':
+          case 'linkedin':
+            this.socialMedia.push({name: this.translateSocial.links[key].name, icon: this.translateSocial.links[key].icon, href: this.translateSocial.links[key].link});
+            break;
+          case 'email':
+            this.socialMedia.push({name: this.translateSocial.links[key].name, icon: this.translateSocial.links[key].icon, href: 'mailto:' + this.translateSocial.links[key].link});
+            break;
+        }
+      }
+    });
+  }
 }
